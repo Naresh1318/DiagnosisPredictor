8a19c51584c73c14ab8ccb71b8c4e1e01cb4bb7c
naresh1318 <rnaresh.n@gmail.com>
2017-05-08 19:05:25 +0530

    added the new website image

--- Project_Website/app.py
+++ Project_Website/app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 # Parameters for Flask-mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'compvisionnn@gmail.com'
-app.config['MAIL_PASSWORD'] = 'chzmbufftnjnyfah'
+app.config['MAIL_USERNAME'] = 'email'
+app.config['MAIL_PASSWORD'] = 'password'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
